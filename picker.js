9f24ec15ad6f11e7d33c4c603e9d8f4994b22da7
Boris Kuznecov <bkuznetsov@ddestiny.ru>
2013-04-21 13:33:08 +0400

    init commit

---
+++ picker.js
@@ -0,0 +1,20 @@
+$(function(){
+    var openPicker   = function(e){
+        var mainWindow  = $('#pick-window');
+
+        if (mainWindow.css('display') == 'none'){
+            mainWindow.show();
+        }else if (mainWindow.css('display') == 'block'){
+            mainWindow.hide();
+        };
+
+        setTimeout(function(){
+            $('#pick-loader').hide();
+        }, 5000)
+    };
+
+
+    var pickButton  = $('#pick-button');
+
+    pickButton.click(openPicker);
+})
